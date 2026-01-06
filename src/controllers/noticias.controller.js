0af69a70e544cbe5b5bc763d52edcb089c73b2f4
camilo-atb <camilotellez1821@gmail.com>
2026-01-06 15:41:40 -0500

    nuevo cambio update news

--- src/controllers/noticias.controller.js
+++ src/controllers/noticias.controller.js
@@ -40,7 +40,7 @@ const modificarNoticia = async (req, res, next) => {
       return res.status(404).json({ error: "Usuario no encontrado en la base de datos" });
     }
 
-    const { id } = req.params;
+    const id = Number(req.params.id);
 
     const updateNoticia = await noticiasService.editarNoticia(id, req.body);
     res.status(200).json(updateNoticia);
