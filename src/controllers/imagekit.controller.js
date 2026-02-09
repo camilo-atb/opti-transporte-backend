25a40eac47ceb2bde8c6f5447ff47a0e8bf3ca43
Camilo Téllez <camilotellez1821@gmail.com>
2026-02-08 21:43:37 -0500

    imagekit

--- src/controllers/imagekit.controller.js
+++ src/controllers/imagekit.controller.js
@@ -1,8 +1,15 @@
 import imagekit from "../config/imagekit.js";
 
 const getAuthParams = (req, res) => {
-  const authParams = imagekit.getAuthenticationParameters();
-  res.json(authParams);
+  try {
+    const authParams = imagekit.getAuthenticationParameters();
+    res.json(authParams);
+  } catch (error) {
+    console.error("IMAGEKIT AUTH ERROR:", error);
+    res.status(500).json({
+      error: "Error generando autenticación de ImageKit",
+    });
+  }
 };
 
 export default { getAuthParams };
