48782972f7fa4c558bb380f77e37830b6670aadc
CrowHush <crowhush@gmail.com>
2026-02-22 09:56:12 -0500

    Cmabio supabase

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrlBr = process.env.SUPABASE_URL_BR;
 const supabaseServiceRolKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseServiceRolKey);
+const supabase = createClient(supabaseUrlBr, supabaseServiceRolKey);
 export default supabase;
